7b108d2750d8f27a49714ac2c7cbc6be7e26a7a8
AlexSolizDuran <solizelex1999@gmail.com>
2024-09-01 22:08:49 -0400

    clase figura actualizada

--- open/clase poligono/centro relativo/CfiguraT.cs
+++ open/clase poligono/centro relativo/CfiguraT.cs
@@ -9,62 +9,82 @@ namespace centro_relativo
 {
     internal class CfiguraT
     {  
+        private Dictionary<string, CObjeto> Objeto = new Dictionary<string, CObjeto>();
         private float[] _Vertices ;
         private uint[] _Indices;
+        private float[] _Centroide = new float[3];
 
-        public CfiguraT(float X, float Y, float Z)
-        {                    //alto,largo,prof   
-            Ccubo cubo1 = new(0.3f, 0.7f, 0.2f);
-            _Indices = cubo1.GetIndices();
-            _Vertices = cubo1.GetVertices();
+        public CfiguraT(float X, float Y, float Z)  
+        {
+            Objeto.Add("Cubo 1", new CObjeto(0.3f, 0.7f, 0.2f));
+            Objeto.Add("Cubo 2", new CObjeto(0.6f, 0.3f, 0.2f));
 
-            //Ccubo cubo2 = new(0.6f, 0.3f, 0.2f );
-            //JuntarInd(cubo2.GetIndices());
-            //JuntarVer(cubo2.GetVertices());
+            Objeto["Cubo 1"].Mov_Centroide(0.0f, 0.45f, 0.0f);
+            Objeto["Cubo 2"].Mov_Centroide(0.0f, 0.0f, 0.0f);
+            JuntarVer();
+            JuntarInd();
+            Juntar_Centroide();
 
         }
-        //junta los vertices
-        private void JuntarVer(float[] ver2)
+        public void Mover_Centroide(float X, float Y,float Z)
         {
-            int cant = Nvert();
-            int cant1 = ver2.Length;
-            if (cant < cant + cant1)
+            
+            foreach(KeyValuePair<string, CObjeto> kvp in Objeto)
             {
-                Array.Resize(ref _Vertices, cant + cant1);
+                float X2 = X + kvp.Value.PosCentroide(0);
+                float Y2 = Y + kvp.Value.PosCentroide(1);
+                float Z2 = Z + kvp.Value.PosCentroide(2);
+                kvp.Value.Mov_Centroide(X2,Y2,Z2);
             }
+            
+            JuntarVer();
+            Juntar_Centroide();
+        }
+        public void Juntar_Centroide()
+        {
+            for (int i = 0; i < 3;i++)
+            {
+                float suma = 0;
+                foreach (KeyValuePair<string, CObjeto> kvp in Objeto)
+                {
 
-            for (int i = 0; i < cant1; i++)
+                    suma += kvp.Value.GetCentroide()[i];
+                }
+                _Centroide[i] = suma / Objeto.Count;
+            }  
+        }
+        //junta los vertices
+        private void JuntarVer()
+        {
+            List<float> listtemp = new List<float>();
+            foreach (KeyValuePair<string,CObjeto>kvp in Objeto)
             {
-                InserVer(cant + i, ver2[i]);
+                listtemp.AddRange(kvp.Value.GetVertices());
             }
+            _Vertices = listtemp.ToArray();
 
         }
         //junta los indices
-        private void JuntarInd(uint[] ind2)
+        private void JuntarInd()
         {
-            int cant = Nindi();
-            int cant1 = ind2.Length;
-            if (cant < cant + cant1)
+            List<uint> listtemp = new List<uint>();
+            uint suma = 0;
+            foreach (KeyValuePair<string, CObjeto> kvp in Objeto)
             {
-                Array.Resize(ref _Indices, cant + cant1);
-            }
+                uint[] indices = kvp.Value.GetIndices();
+                for (int i = 0; i < indices.Length; i++)
+                {
+                    listtemp.Add(indices[i] + suma);
+                }
+                suma = (uint)listtemp.ToArray().Length;
 
-            for (int i = 0; i < cant1; i++)
-            {
-                InserInd(cant + i, ind2[i] + (ind2.Max() + 1));
+
             }
+            
+            _Indices = listtemp.ToArray();
         }
 
-        //inserta un uint a una posicion de el arreglo de indices
-        private void InserInd(int pos, uint elem) { _Indices[pos] = elem; }
-        //inserta un float a una posicion de el arreglo de vertices
-        private void InserVer(int pos, float elem) { _Vertices[pos] = elem; }
-        //cantidad de los vertices
-        public int Cant_Vert() { return _Indices.Length / 3; }
-        //cantidad de los elementos del arreglo de los indices
-        public int Nindi() { return _Indices.Length; }
-        //cantidad de los elementos del arreglo de vertices
-        public int Nvert() { return _Vertices.Length; }
+        
 
         public float[] GetVertices() { return _Vertices; }
         public uint[] GetIndices() { return _Indices; }
